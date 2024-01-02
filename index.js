f6be9f053143e2601cf43f3ef35c557039f658af
siraj9949 <shaiksirajuddin9949@gmail.com>
2024-01-02 10:57:37 +0530

    .env

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ import path from "path";
 
 import { Student } from "./dbase/schemas.js";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
